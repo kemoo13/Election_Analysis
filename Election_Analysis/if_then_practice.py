4ddd0407e2e5a54c5f9c8316d874ebe8e2535d26
kemoo13 <105181519+kemoo13@users.noreply.github.com>
2022-06-15 19:54:33 -0400

    Add files via upload

---
+++ Election_Analysis/if_then_practice.py
@@ -0,0 +1,14 @@
+:
+temperature = int(input("What is the temperature outside?"))
+
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows")
+
+#What is the score?
+score = int(input("What is your test score?"))
+
+#Determine the grade.
+if score >= print('Your grade is an A."')
